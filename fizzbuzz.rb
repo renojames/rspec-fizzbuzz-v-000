6718beaa62acd06ef06a020b8c9bb58c213c0370
Reno Sorensen <renojames19@gmail.com>
2017-11-15 02:40:24 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,9 +2,9 @@ def fizzbuzz(int)
   if int % 3 == 0
     "Fizz"
   elsif int % 5 == 0
-    puts 'Buzz'
+    "Buzz"
   elsif int % 3 == 0 && int % 5 == 0
-    puts 'FizzBuzz'
+    "FizzBuzz"
   else
     puts 'nil'
   end
